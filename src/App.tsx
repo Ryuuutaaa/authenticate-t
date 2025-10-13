a744933f166312d6ac1187b57d6666ec0f3301af
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-10-13 22:35:57 +0800

    feat: add background index

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div className="underline text-center">test</div>;
+  return (
+    <div className="font-sans bg-[#f1d4b3] min-h-screen">
+      <h1 className="text-[120px] font-bold text-black/5 uppercase absolute left-1/2 -translate-x-1/2">
+        TodoApp
+      </h1>
+    </div>
+  );
 };
 
 export default App;
