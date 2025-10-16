f0e66e23bec7e0e3615e648e65d73de74cea7c7b
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-10-16 23:33:16 +0800

    feat: adjust button components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return<button type="submit" className="h-[45x] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer">Add to list</button>
-}
+const Button = ({ children, buttonType }) => {
+  return (
+    <button
+      type="submit"
+      className={`h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer ${
+        buttonType === "secondary" ? "opacity-[85%]" : ""
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
