8acc411ce0bb5f9bc7d41e733598f8932dc9e539
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-10-21 16:46:09 +0800

    feat:  add initial todos by arrya object

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,25 @@
 import ButtonDelete from "./ButtonDelete";
 
-const initialTodos = ["By grocies", "walk the dog", "laudry"];
+const initialTodos = [
+  { text: "By grocies", isCompleted: false },
+  { text: "walk the dog", isCompleted: false },
+  { text: "laudry", isCompleted: true },
+];
 
 const TodoList = () => {
   return (
     <ul>
       {initialTodos.map((todo) => (
         <li
-          key={todo}
+          key={todo.text}
           className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
         >
-          <span>{todo}</span> <ButtonDelete />
+          <span
+            className={`${todo.isCompleted ? "line-through text-[#ccc]" : ""}`}
+          >
+            {todo.text}
+          </span>{" "}
+          <ButtonDelete />
         </li>
       ))}
     </ul>
