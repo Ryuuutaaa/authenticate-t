d867779c1454d689c9a40de08309d692ce0ba3cc
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-10-21 16:47:24 +0800

    feat: add usetstate todolisst just call

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,15 @@
+import { useState } from "react";
 import ButtonDelete from "./ButtonDelete";
 
-const initialTodos = [
-  { text: "By grocies", isCompleted: false },
-  { text: "walk the dog", isCompleted: false },
-  { text: "laudry", isCompleted: true },
-];
-
 const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { text: "By grocies", isCompleted: false },
+    { text: "walk the dog", isCompleted: false },
+    { text: "laudry", isCompleted: true },
+  ]);
   return (
     <ul>
-      {initialTodos.map((todo) => (
+      {todos.map((todo) => (
         <li
           key={todo.text}
           className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
