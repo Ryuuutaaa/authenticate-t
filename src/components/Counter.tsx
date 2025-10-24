e3453852325a474cb2a06c487fe19fba3483d7fa
rafi.widikta <rafi.widikta@incentro.com>
2025-10-24 15:39:08 +0800

    feat: add count todo iscomplated count

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,7 @@
 const Counter = ({ todos }) => {
   return (
     <p>
-      <b>0</b> / {todos.length} todos complated
+      <b>{todos.filter((todo) => todo.isCompleted).length}</b> / {todos.length} todos complated
     </p>
   );
 };
